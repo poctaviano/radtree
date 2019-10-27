56de08522d9badc1424571b3b94ec1a13793e63e
Pedro Octaviano <45361504+poctaviano@users.noreply.github.com>
2019-10-27 18:46:55 +0100

    init

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='radial-tree',
-    version='0.1',
-    scripts=['radtree']                 
+    description='Radial Plot for sklearn Decision Trees',
+    version='0.0.1',
+    url='git@github.com:poctaviano/radtree.git',
+    author='Pedro Octaviano',
+    author_email='pedro.octaviano@gmail.com',
+    license='MIT License',
+    # scripts=['radtree'],
+    # packages=['radtree.py'], find_namespace_packages(include=['mynamespace.*'])
+    packages=find_namespace_packages(include=['radtree.*']),
+    zip_safe=False
 )
