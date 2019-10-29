5590de5683089b7c380887459b3ba0d49faed5ef
Pedro Octaviano <45361504+poctaviano@users.noreply.github.com>
2019-10-29 11:47:17 +0100

    V0.0.3 - cleanup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='radtree',
     description='Radial Plot for sklearn Decision Trees',
-    version='0.0.2',
+    version='0.0.3',
     url='http://github.com/poctaviano/radtree',
     author='Pedro Octaviano',
     author_email='pedrooctaviano@gmail.com',
