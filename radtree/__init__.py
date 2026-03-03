22de64bbcca6c172aec188e48d96949fe6b0648e
P. Octaviano <45361504+poctaviano@users.noreply.github.com>
2026-03-03 16:07:23 +0100

    fix: stabilize plot_radial and quick_fitted_tree on modern pandas/sklearn

--- radtree/__init__.py
+++ radtree/__init__.py
@@ -1,4 +1,9 @@
-from .radtree import *
+from .radtree import (
+    plot_pca,
+    plot_radial,
+    plot_tsne,
+    plot_umap,
+    quick_fitted_tree,
+)
 
-
-# __all__ = [plot_radial, plot_pca, plot_tsne, plot_umap, quick_fitted_tree]
+__all__ = ["plot_radial", "plot_pca", "plot_tsne", "plot_umap", "quick_fitted_tree"]
